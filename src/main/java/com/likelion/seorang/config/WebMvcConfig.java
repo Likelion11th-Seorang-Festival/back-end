cbe6e1cd1d2cbf2ebf92351bfd76c4a27942616a
Jungmin-Hong <jungmin011107@gmail.com>
2023-05-22 12:21:14 +0900

    refactor: fixing security error

--- src/main/java/com/likelion/seorang/config/WebMvcConfig.java
+++ src/main/java/com/likelion/seorang/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
                 .allowedOrigins("http://localhost:3000") // 허용할 Origin 설정
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE", "HEAD") // 허용할 HTTP 메서드 설정
-                .allowedHeaders("*") // 허용할 요청 헤더 설정
-                .allowCredentials(true); // 인증 정보 허용 여부
+                .allowedHeaders("*"); // 허용할 요청 헤더 설정
+//                .allowCredentials(true); // 인증 정보 허용 여부
     }
 }
