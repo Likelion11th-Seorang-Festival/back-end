f411b67931fe768b6c25e3990845d91b8ab96a4e
Jungmin-Hong <jungmin011107@gmail.com>
2023-05-22 12:34:05 +0900

    refactor: fixing security error

---
+++ src/main/java/com/likelion/seorang/config/CustomCorsFilter.java
@@ -0,0 +1,20 @@
+package com.likelion.seorang.config;
+
+import org.springframework.stereotype.Component;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomCorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "*");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        chain.doFilter(request, response);
+    }
+}
