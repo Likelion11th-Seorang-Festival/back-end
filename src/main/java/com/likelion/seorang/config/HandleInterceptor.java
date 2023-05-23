de58e1e5d1b4f20440930d9f6e4cffc397373371
Jungmin-Hong <jungmin011107@gmail.com>
2023-05-24 00:21:22 +0900

    refactor: fix cors error

---
+++ src/main/java/com/likelion/seorang/config/HandleInterceptor.java
@@ -0,0 +1,19 @@
+package com.likelion.seorang.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HandleInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
+        return false;
+    }
+
+}
